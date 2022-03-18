c5555cfee1e2123dc9b23854dfab8a60816d313e
Sylruilshu <101709517+Sylruilshu@users.noreply.github.com>
2022-03-19 01:44:38 +1100

    Add `chapter-2` (#1)
    Adds examples, answers and diagrams for chapter 2.

---
+++ chapter-2/swordfish2.py
@@ -0,0 +1,10 @@
+name = ''
+password = ''
+
+while name != 'foo':
+    print('Who are you?')
+    name = input()   
+while password != 'bar':
+    print('Hello foo, what is your password?')
+    password = input()
+print('Access granted')
